489e83ff0670ce790e00a91d91e7f1fd18a55e62
t3nq <50250208+t3nq@users.noreply.github.com>
2025-04-27 16:54:32 +0300

    Update main.py
    Переход к функциональному методу программирования

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import random
 
+def wer(a, b):
+    j = random.randint(a,b)
+    return j
 
 a = int(input("введите число"))
-b = int(input("введите число"))
-j = random.randint(a,b)
-print("Ваше число:",j)
+b = int(input("введите число")) 
+res = wer(a,b)
+print("Ваше число:", res)
