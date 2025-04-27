b33a7af30a6f18c3f2b317636a2ffd0439ee393e
t3nq <50250208+t3nq@users.noreply.github.com>
2025-04-27 17:11:13 +0300

    Update main.py
    Переход к ООП

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import random
 
-def wer(a, b):
-    j = random.randint(a,b)
-    return j
+
+class Randomizer:
+    def __init__(self, x, y):
+        self.border1 = x
+        self.border2 = y
+        self.value = self.__get_random_value()
+
+    def __get_random_value(self):
+        return random.randint(self.border1, self.border2)
+
+    def get_value(self):
+        return self.value
+
 
 a = int(input("введите число"))
 b = int(input("введите число")) 
-res = wer(a,b)
-print("Ваше число:", res)
+randomizer = Randomizer(a, b)
+result = randomizer.get_value()
+print("Ваше число:", result)
